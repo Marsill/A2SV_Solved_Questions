58f36910308376e064a434020c434dce75db1bab
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2026-03-16 02:42:05 -0500

    Add solution for Binary Subarrays With Sum

---
+++ leetcode/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def helper(x):
+            if x < 0: return 0
+            
+            res = 0
+            l, curr_sum = 0, 0
+
+            for r in range(len(nums)):
+                curr_sum += nums[r]
+                while curr_sum > x:
+                    curr_sum -= nums[l]
+                    l += 1
+                res += (r-l+1)
+            return res
+
+        return helper(goal) - helper(goal - 1)
