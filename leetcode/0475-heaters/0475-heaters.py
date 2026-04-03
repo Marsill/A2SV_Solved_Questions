c5683cfce9efd94eac153d88ee0a6544ae12dd87
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2026-04-03 11:46:09 -0500

    Add solution for Heaters

--- leetcode/0475-heaters/0475-heaters.py
+++ leetcode/0475-heaters/0475-heaters.py
@@ -1,36 +1,16 @@
 class Solution:
-    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
-        heaters.sort()
-        
-        def binary_search(arr, target):
-            left, right = 0, len(arr) - 1
-            ans = len(arr)  
-            
-            while left <= right:
-                mid = (left + right) // 2
-                
-                if arr[mid] >= target:
-                    ans = mid
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            
-            return ans
-        
-        radius = 0
-        
-        for house in houses:
-            i = binary_search(heaters, house)
-            
-            left_dist = float('inf')
-            if i - 1 >= 0:
-                left_dist = house - heaters[i - 1]
-            
-            right_dist = float('inf')
-            if i < len(heaters):
-                right_dist = heaters[i] - house
-            
-            closest = min(left_dist, right_dist)
-            radius = max(radius, closest)
-        
-        return radius
+    def findRadius(self, ho: List[int], he: List[int]) -> int:
+        ho.sort()
+        he.sort()
+        ans = float('-inf')
+        he.append(float('inf'))
+        for house in ho:
+            idx = bisect_left(he, house)
+            mine = float('inf')
+            mine = min(mine, abs(house - he[idx]))
+            if idx + 1 < len(he):
+                mine = min(mine, abs(house - he[idx + 1]))
+            if idx - 1 >= 0:
+                mine = min(mine, abs(house - he[idx - 1]))
+            ans = max(ans, mine)
+        return ans
