594bb50a5af68e030de0712da116062e0fcfafac
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2026-05-01 17:31:19 -0500

    Add solution for Is Graph Bipartite?

---
+++ leetcode/0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        
+        color = [-1] * n
+        
+        def dfs(node, c):
+            color[node] = c
+            
+            for nei in graph[node]:
+                if color[nei] == c:
+                    return False
+                
+                if color[nei] == -1:
+                    if not dfs(nei, 1 - c):
+                        return False
+            
+            return True
+        
+        for node in range(n):
+            if color[node] == -1:
+                if not dfs(node, 0):
+                    return False
+        
+        return True
